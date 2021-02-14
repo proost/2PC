546faaa7fe947e5c72f5456a5297b0991a850141
proost <jwalag87@gmail.com>
2021-02-15 02:08:57 +0900

    init 2pc project

---
+++ coordinator/src/main/java/listener/Server.java
@@ -0,0 +1,17 @@
+package listener;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Server {
+
+    public static void main(String[] args) {
+        try {
+            while (true) {
+
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+}
